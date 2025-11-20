85fcfa2d344b44f9a764d491b1a05c8fd8aaa06c
unknown <abdurrahmanaikon@gmail.com>
2025-11-20 11:00:20 +0700

    menambahkan routinh api dan middwale jwt

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,22 +3,37 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
-
+use Tymon\JWTAuth\Http\Middleware\Authenticate as JWTMiddleware;
+use Illuminate\Foundation\Configuration\Routing;
+use App\Http\Middleware\Authenticate;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+            api: __DIR__.'/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
+
+   
+
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
     })
+      ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'jwt' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+        ]);
+    })
+    //   ->withRoutes(function (Routing $router) {
+    //     $router->web();   // untuk routes/web.php
+    //     $router->api();   // 🟢 WAJIB kalau mau routes/api.php terbaca
+    // })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
